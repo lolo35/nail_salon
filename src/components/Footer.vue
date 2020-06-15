579f6080c93c5b46c5fee4f4a7bc55ec9defb50d
Raul Filimon <raul.filimon@autoliv.com>
2020-06-15 21:08:37 +0300

    some work on footer, still no satisfied

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,30 +1,56 @@
 <template>
-    <div class="container-fluid bg-success p-5">
+    <div class="container-fluid bg-success pb-5">
         <div class="row">
-            <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4"></div>
-            <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4"></div>
-            <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4"></div>
+            <div class="col-12">
+                <div class="container mt-5 mb-5">
+                    <div class="row">
+                        <div class="col-12 col-sm-12 col-md-6 col-lg-8 col-xl-8">
+                            <h3>About</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est rem vero at sit quisquam distinctio laboriosam molestias dolor. Accusamus ducimus quibusdam iusto voluptates error facilis commodi maiores! Quis eligendi corporis, necessitatibus ut quia aperiam omnis nihil, ratione eum explicabo sunt quibusdam dicta laudantium voluptate laboriosam, voluptas ullam autem. Natus, necessitatibus?</p>
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-3 col-lg-2 col-xl-2 text-center text-sm-left mb-3 mb-sm-0">
+                            <h3>Categorii</h3>
+                            <h5><a href="#">Link 1</a></h5>
+                            <h5><a href="#">Link 1</a></h5>
+                            <h5><a href="#">Link 1</a></h5>
+                            <h5><a href="#">Link 1</a></h5>
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-3 col-lg-2 col-xl-2 text-center text-sm-left">
+                            <h3>Links</h3>
+                            <h5><a href="#">Link 1</a></h5>
+                            <h5><a href="#">Link 1</a></h5>
+                            <h5><a href="#">Link 1</a></h5>
+                            <h5><a href="#">Link 1</a></h5>
+                        </div>
+                    </div>
+                    <div class="row">
+                        
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="row text-center mt-5">
-            <div class="col-3">
-                <h1 class="text-danger">
-                    <i class="fab fa-youtube"></i>
-                </h1>
+        <div class="row">
+            <div class="col-12">
+                <hr>
             </div>
-            <div class="col-3">
-                <h1 class="text-primary">
-                    <i class="fab fa-facebook-square"></i>
-                </h1>
-            </div>
-            <div class="col-3">
-                <h1 class="text-primary">
-                    <i class="fab fa-twitter"></i>
-                </h1>
-            </div>
-            <div class="col-3">
-                <h1 class="text-info">
-                    <i class="fab fa-google-plus-g"></i>
-                </h1>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8 mt-4">
+                            Copyright &copy; 2020 Toate drepturile rezervate de <a href="#">BitByBit</a>
+                        </div>
+                        <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+                            <div class="d-flex w-100 justify-content-between">
+                                <a href="#" class="fab fa-facebook"></a>
+                                <a href="#" class="fab fa-twitter"></a>
+                                <a href="#" class="fab fa-instagram"></a>
+                                <a href="#" class="fab fa-google-plus-g"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -37,5 +63,20 @@ export default {
 </script>
 
 <style scoped>
+    a{
+        color: #000000;
+    }
+    a:hover{
+        color: #3366cc;
+        text-decoration: none;
+    }
 
+    .fab{
+        padding: 20px;
+        font-size: 20px;
+        width: 30px;
+        text-align: center;
+        text-decoration: none;
+        border-radius: 50%;
+    }
 </style>
